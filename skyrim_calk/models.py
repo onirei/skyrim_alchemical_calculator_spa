af3c47f3989490c2c2cfa89ad91bb1c58cac87f5
onirei <occult.remi@gmail.com>
2019-03-05 23:52:30 +0300

    v1.0.0

--- skyrim_calk/models.py
+++ skyrim_calk/models.py
@@ -2,10 +2,11 @@ import sqlite3
 from django import forms
 
 
-class NameForm(forms.Form):
+class CalculateForm(forms.Form):
 
-
-    def selectors(self):
+    #наполнение списка выборов для формы из бд
+    @staticmethod
+    def selectors():
         select = (('', '',),)
         conn = sqlite3.connect("Skyrim.sqlite3")
         cursor = conn.cursor()
@@ -23,18 +24,7 @@ class NameForm(forms.Form):
             select += ((result[i][0], result[i][0],),)
         return select
 
-
-    CHOICES = selectors(forms.Form)
-    attrib_1 = forms.ChoiceField(label='', choices=CHOICES, required=True)
-    attrib_2 = forms.ChoiceField(label='', choices=CHOICES, required=False)
-    attrib_3 = forms.ChoiceField(label='', choices=CHOICES, required=False)
-    attrib_4 = forms.ChoiceField(label='', choices=CHOICES, required=False)
-
-    CHOICES = (('0', 'Поиск',), ('1', 'Оптимизация',))
-    choice_field = forms.ChoiceField(label='Режим', widget=forms.RadioSelect, choices=CHOICES, initial='0')
-    CHOICES2 = (('0', 'Положительные',), ('1', 'Отрицательные',))
-    choice_field_last = forms.ChoiceField(label='Ключевой эффект', widget=forms.RadioSelect, choices=CHOICES2, initial='0')
-
+    #простой поиск по бд
     @staticmethod
     def connection_to_bd(simplifier):
         conn = sqlite3.connect("Skyrim.sqlite3")
@@ -69,54 +59,59 @@ class NameForm(forms.Form):
         conn.close()
         return result
 
+    #простой поиск по бд (ищет от 1 до 4 атрибутов в запросе)
     @staticmethod
     def finder (attribute_1, attribute_2, attribute_3, attribute_4):
         attributes = (attribute_1, attribute_2, attribute_3, attribute_4)
         attributes = tuple(filter(None, attributes))
         # if not attributes: attributes = ('',) //проверка на все пустые поля, убрана так как 1 поле обязательное
-        bd_result = NameForm.connection_to_bd(attributes)
+        bd_result = CalculateForm.connection_to_bd(attributes)
         return bd_result
 
+    #поиск по бд с последующим поиском (ищет по ключевым атрибутам, потом ищет по дополнительным атрибутам)
     @staticmethod
     def optimizer (attribute_1, attribute_2, attribute_3, attribute_4, bd_data):
         attributes = (attribute_1, attribute_2, attribute_3, attribute_4)
         attributes = tuple(filter(None, attributes))
-        bd_result = NameForm.connection_to_bd(attributes)
+        bd_result = CalculateForm.connection_to_bd(attributes)
 
+        #получаем набор элементов по условию
         conn = sqlite3.connect("Skyrim.sqlite")
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM {0}".format(bd_data))
         bd_data = cursor.fetchall()
         conn.close()
 
-
+        #init
         bd_result_optima = []
         bd_result_optima_extend = []
 
+        #определяем основные и вторичные атрибуты, производим поиск по вторичным
         for ingredient in bd_result:
             attributes_optima = ()
             for attribute in ingredient:
                 if ((attribute,) in bd_data) and (attribute not in attributes):
                     attributes_optima += (attribute,)
             if len(attributes_optima) == 1:
-                bd_result_optima += NameForm.connection_to_bd(attributes_optima)
+                bd_result_optima += CalculateForm.connection_to_bd(attributes_optima)
             elif len(attributes_optima) == 2:
-                bd_result_optima += NameForm.connection_to_bd(attributes_optima)
-                bd_result_optima += NameForm.connection_to_bd((attributes_optima[0],))
-                bd_result_optima += NameForm.connection_to_bd((attributes_optima[1],))
+                bd_result_optima += CalculateForm.connection_to_bd(attributes_optima)
+                bd_result_optima += CalculateForm.connection_to_bd((attributes_optima[0],))
+                bd_result_optima += CalculateForm.connection_to_bd((attributes_optima[1],))
             elif len(attributes_optima) == 3:
-                bd_result_optima += NameForm.connection_to_bd(attributes_optima)
-                bd_result_optima += NameForm.connection_to_bd((attributes_optima[0],))
-                bd_result_optima += NameForm.connection_to_bd((attributes_optima[1],))
-                bd_result_optima += NameForm.connection_to_bd((attributes_optima[2],))
-                bd_result_optima += NameForm.connection_to_bd(attributes_optima[0:1])
-                bd_result_optima += NameForm.connection_to_bd(attributes_optima[1:2])
-                bd_result_optima += NameForm.connection_to_bd(attributes_optima[1:3:2])
+                bd_result_optima += CalculateForm.connection_to_bd(attributes_optima)
+                bd_result_optima += CalculateForm.connection_to_bd((attributes_optima[0],))
+                bd_result_optima += CalculateForm.connection_to_bd((attributes_optima[1],))
+                bd_result_optima += CalculateForm.connection_to_bd((attributes_optima[2],))
+                bd_result_optima += CalculateForm.connection_to_bd(attributes_optima[0:1])
+                bd_result_optima += CalculateForm.connection_to_bd(attributes_optima[1:2])
+                bd_result_optima += CalculateForm.connection_to_bd(attributes_optima[1:3:2])
             else:
                 pass
             bd_result_optima += (('', '', '', '', '', '',),)
         bd_result_optima = bd_result + [('', '', '', '', '', '',),] + bd_result_optima
 
+        #еб*сь разметка конём, убираем повторы в массиве
         for i in bd_result_optima:
             if (i not in bd_result_optima_extend) and (i != ('', '', '', '', '', '',)):
                 bd_result_optima_extend.append(i)
@@ -130,6 +125,18 @@ class NameForm(forms.Form):
                 bd_result_optima.append(bd_result_optima_extend[i])
 
         return bd_result_optima
+
+    CHOICES = selectors
+    attrib_1 = forms.ChoiceField(label='', choices=CHOICES, required=True)
+    attrib_2 = forms.ChoiceField(label='', choices=CHOICES, required=False)
+    attrib_3 = forms.ChoiceField(label='', choices=CHOICES, required=False)
+    attrib_4 = forms.ChoiceField(label='', choices=CHOICES, required=False)
+
+    CHOICES = (('0', 'Поиск',), ('1', 'Оптимизация',))
+    choice_field = forms.ChoiceField(label='Режим', widget=forms.RadioSelect, choices=CHOICES, initial='0')
+    CHOICES2 = (('0', 'Положительные',), ('1', 'Отрицательные',))
+    choice_field_last = forms.ChoiceField(label='Ключевой эффект', widget=forms.RadioSelect, choices=CHOICES2,
+                                          initial='0')
 
 
 
--- skyrim_calk/views.py
+++ skyrim_calk/views.py
@@ -1,26 +1,30 @@
 from django.shortcuts import render
-from .models import NameForm
+from .models import CalculateForm
 
 
 def get_name(request):
     if request.method == 'POST':
-        form = NameForm(request.POST)
+        form = CalculateForm(request.POST)
         if form.is_valid():
+            #определяем набор элементов
             if form.cleaned_data["choice_field_last"] == '0':
                 bd_data = 'Positive'
-            elif form.cleaned_data["choice_field_last"] == '1':
+            # elif form.cleaned_data["choice_field_last"] == '1':
+            else:
                 bd_data = 'Negative'
 
+            #определяем режим выборки
             if form.cleaned_data["choice_field"] == '0':
                 data = form.finder(form.cleaned_data["attrib_1"], form.cleaned_data["attrib_2"], form.cleaned_data[
                     "attrib_3"], form.cleaned_data["attrib_4"])
-            elif form.cleaned_data["choice_field"] == '1':
+            # elif form.cleaned_data["choice_field"] == '1':
+            else:
                 data = form.optimizer(form.cleaned_data["attrib_1"], form.cleaned_data["attrib_2"], form.cleaned_data[
                     "attrib_3"], form.cleaned_data["attrib_4"], bd_data)
 
             return render(request, 'skyrim_calk/index.html', {'form': form, 'data': data})
     else:
-        form = NameForm()
+        form = CalculateForm()
 
     return render(request, 'skyrim_calk/index.html', {'form': form})
 
